e13bf22330cc98c5928b93433e19b3f29ac722bc
Christian Albert <c.albert@shippio.io>
2022-08-28 21:34:45 +0900

    manually lint before writing bookmarklet bundle to file

--- bookmarklet/build.ts
+++ bookmarklet/build.ts
@@ -1,14 +1,30 @@
 import { exit } from "https://deno.land/std@0.133.0/node/process.ts";
-import { build } from "../deps.ts";
+import { build, writeFile } from "../deps.ts";
+
+const BUILD_FILEPATH = "./bookmarklet/build/test.js";
 
 const output = await build({
+  platform: "browser",
+  format: "iife",
   entryPoints: ["./bookmarklet/src/index.js"],
-  outfile: "./bookmarklet/build/test.js",
+  target: ["es2020"],
+  outfile: BUILD_FILEPATH,
   bundle: true,
   minifyWhitespace: true,
   minifySyntax: true,
+  // disable automatic write-to-file so output can be manipulated
+  write: false,
 });
 
-console.log(" build finished", JSON.stringify(output));
+const { errors, warnings, outputFiles } = output;
+console.log(" build finished", JSON.stringify({ errors, warnings }));
+
+// I couldn't get ESBuild to use only const. It really wants to insert `var` and `let`
+// Manually writing to file via `write: false` enables access to output text for further "linting".
+const text = outputFiles?.[0]?.text;
+if (text) {
+  const linted = text.replace(/(var|let)\s/g, "const ");
+  await writeFile(BUILD_FILEPATH, linted);
+}
 
 exit(0);
--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 export { json, opine } from "https://deno.land/x/opine@2.2.0/mod.ts";
 export { Database } from "https://deno.land/x/aloedb@0.9.0/mod.ts";
 export { build } from "https://deno.land/x/esbuild@v0.15.5/mod.js";
-export { readFile } from "https://deno.land/std@0.153.0/node/fs/promises.ts";
+export { writeFile } from "https://deno.land/std@0.153.0/node/fs/promises.ts";
